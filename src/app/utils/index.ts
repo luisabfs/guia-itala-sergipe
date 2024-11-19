3ee1ef900f1e59603c1a46da9f01eb90ea6a1d27
Luisa Barros <luisa.barros.dev@gmail.com>
2024-11-19 20:44:53 -0300

    chore: add formatTime helper function

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -1,12 +1,15 @@
 export function slugify(text: string) {
-    return text
-      .toLowerCase()
-      .normalize('NFD') // Normalize to decompose accents
-      .replace(/[\u0300-\u036f]/g, '') // Remove accents
-      .replace(/[^a-z0-9\s-]/g, '') // Remove invalid characters
-      .trim()
-      .replace(/\s+/g, '-') // Replace spaces with hyphens
-      .replace(/-+/g, '-'); // Remove consecutive hyphens
-  }
-  
-  
+  return text
+    .toString()
+    .toLowerCase()
+    .normalize('NFD') // Normalize to decompose accents
+    .replace(/[\u0300-\u036f]/g, '') // Remove accents
+    .replace(/[^a-z0-9\s-]/g, '') // Remove invalid characters
+    .trim()
+    .replace(/\s+/g, '-') // Replace spaces with hyphens
+    .replace(/-+/g, '-'); // Remove consecutive hyphens
+}
+
+export function formatTime(timeString: string) {
+  return timeString.slice(0, 5);  // This takes the first five characters ("17:00")
+};
