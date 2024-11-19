4add60c4a0d3d3207f0423f5810c0b500627aac6
Luisa Barros <luisa.barros.dev@gmail.com>
2024-11-19 20:45:13 -0300

    feat: fetch tours from strapi

--- src/app/roteiros/TourModalContent.tsx
+++ src/app/roteiros/TourModalContent.tsx
@@ -1,41 +1,59 @@
 'use client';
+
+import WhatsappCta from '../components/WhatsappCta';
 import { Tour } from '../types';
+import { formatTime } from '../utils';
 
 export default function TourModalContent({ selectedTour }: { selectedTour: Tour }) {
+  const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+  const backgroundImage = selectedTour.image ? `url(${API_BASE_URL}${selectedTour?.image?.url})` : `url(${selectedTour.imageUrl})`;
+
   return (
     <div className='text-black flex flex-col gap-2'>
-      <p className='text-justify'>{selectedTour.description}</p>
+      <p className='text-justify'>{selectedTour.description ?? ''}</p>
       <div
         className={`relative bg-cover bg-center rounded-lg w-full min-h-[200px]`}
-        style={{ backgroundImage: `url(${selectedTour.imageUrl})` }}
+        style={{ backgroundImage }}
       />
       <div id="schedule" className='flex gap-6 text-sm'>
-        <div className='flex flex-col'>
-          <span>saída</span>
-          <span className='p-2 bg-[#c0dfd3] w-fit rounded-lg font-bold'>{selectedTour.departure}</span>
-        </div>
-        <div className='flex flex-col'>
-          <span>previsão de retorno</span>
-          <span className='p-2 bg-[#c0dfd3] w-fit rounded-lg font-bold'>{selectedTour.returnTime}</span>
-        </div>
+        {selectedTour.departureTime ? (
+          <div className='flex flex-col'>
+            <span>saída</span>
+            <span className='p-2 bg-[#c0dfd3] w-fit rounded-lg font-bold'>{formatTime(selectedTour.departureTime)}</span>
+          </div>
+        ) : null}
+        {selectedTour.returnTime ? (
+          <div className='flex flex-col'>
+            <span>previsão de retorno</span>
+            <span className='p-2 bg-[#c0dfd3] w-fit rounded-lg font-bold'>{formatTime(selectedTour.returnTime)}</span>
+          </div>
+        ) : null}
       </div>
+
       <div className='flex gap-8 md:gap-10'>
-        <div id="included-items">
-          <span className='font-bold'>Incluso:</span>
-          <ul className='text-sm list-disc pl-6'>
-            {selectedTour.included.map(item => (
-              <li>{item}</li>
-            ))}
-          </ul>
-        </div>
-        <div id="non-included-items">
-          <span className='font-bold'>Não Incluso:</span>
-          <ul className='text-sm list-disc pl-6'>
-            {selectedTour.notIncluded.map(item => (
-              <li>{item}</li>
-            ))}
-          </ul>
-        </div>
+        {selectedTour.included && selectedTour.included?.length > 0 ? (
+          <div id="included-items">
+            <span className='font-bold'>Incluso:</span>
+            <ul className='text-sm list-disc pl-6'>
+              {selectedTour.included?.map(item => (
+                <li>{item}</li>
+              ))}
+            </ul>
+          </div>
+        ) : null}
+        {selectedTour.notIncluded && selectedTour.notIncluded?.length > 0 ? (
+          <div id="non-included-items">
+            <span className='font-bold'>Não Incluso:</span>
+            <ul className='text-sm list-disc pl-6'>
+              {selectedTour.notIncluded.map(item => (
+                <li key={item + Math.random()} dangerouslySetInnerHTML={{ __html: item }} />
+              ))}
+            </ul>
+          </div>
+        ) : null}
+
+        {selectedTour.hasCta ? <WhatsappCta /> : null}
+
       </div>
     </div>
   );
--- src/app/roteiros/page.tsx
+++ src/app/roteiros/page.tsx
@@ -1,33 +1,82 @@
 'use client';
 
-import { Card, Header, Modal } from '../components';
+import { Card, Header, LoadingSpinner, Modal } from '../components';
 import { slugify } from '../utils';
 import { useSearchParams, useRouter } from 'next/navigation'
 import TourModalContent from './TourModalContent';
-import tours from '../data/tours.json';
+import fallbackTours from '../data/tours.json';
+import { useEffect, useState } from 'react';
+
+type DataItem = {
+  id: number;
+  documentId: string;
+  title: string;
+  createdAt: string; // ISO date string
+  updatedAt: string; // ISO date string
+  publishedAt: string; // ISO date string
+  locale: string | null;
+};
 
 export default function ToursPage() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const search = searchParams.get('search') ?? '';
 
-  const selectedCard = tours.find((card) => card.id === search);
+  const [tours, setTours] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  // Function to fetch data from the API
+  async function fetchData() {
+    setLoading(true);
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/tours?populate=*`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`
+        },
+      });
+      // Check if the response is okay (status code 200–299)
+      if (!response.ok) {
+        throw new Error(`An error has occurred: ${response.status}`);
+      }
+
+      // Parse the JSON response
+      const { data }: { data: Array<DataItem> } = await response.json();
+      data ? setTours(data) : setTours(fallbackTours);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setTours(fallbackTours);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const selectedCard = tours.find((card) => slugify(card.title) === search);
 
   return (
-    <main className="flex flex-col justify-center items-center bg-[#13271c] font-poppins text-white">
+    <main className="flex flex-col items-center min-h-screen bg-[#13271c] font-poppins text-white">
       <Header />
-      <div className="flex flex-col w-full justify-center items-center gap-8 p-6">
+      <div className="flex flex-1 flex-col w-full items-center gap-8 p-6">
         <h1 className="whitespace-pre-line font-playfair font-bold text-3xl md:text-6xl">
           Roteiros em Sergipe
         </h1>
-        <div className="flex flex-wrap justify-center gap-4 w-full lg:max-w-[900px]">
-          {tours.map((card, index) => (
-            <Card card={card} index={index} length={tours.length} url={`/roteiros?search=${slugify(card.id)}`} />
-          ))}
-        </div>
+        {loading ? (
+          <LoadingSpinner />   
+        ) : (
+          <div className="flex flex-wrap justify-center gap-4 w-full lg:max-w-[900px]">
+            {tours.map((card, index) => (
+              <Card card={card} index={index} length={tours.length} url={`/roteiros?search=${slugify(card.title)}`} />
+            ))}
+          </div>
+        )}
       </div>
 
-      <Modal isOpen={!!search && !!selectedCard} onClose={() => router.push('/roteiros', { scroll: false })} title={selectedCard?.title}>
+      <Modal isOpen={!!selectedCard} onClose={() => router.push('/roteiros', { scroll: false })} title={selectedCard?.title}>
         <TourModalContent selectedTour={selectedCard!} />
       </Modal>
     </main>
