a2ec4f346acbac53c4188666ff2ce75b6d0ae5c0
Luisa Barros <luisa.barros.dev@gmail.com>
2024-11-19 20:43:00 -0300

    feat: add fallback tours validation to card component

--- src/app/components/Card/index.tsx
+++ src/app/components/Card/index.tsx
@@ -1,15 +1,10 @@
 'use client';
 
+import { Tour } from "@/app/types";
 import Link from "next/link";
 
-type Card = {
-  id: number | string;
-  title: string;
-  imageUrl: string;
-}
-
 type CardProps = {
-  card: Card;
+  card: Tour;
   index: number;
   length: number;
   url?: string;
@@ -17,13 +12,16 @@ type CardProps = {
 };
 
 export default function Card({ card, index, length, url = '/roteiros', scroll = false }: CardProps) {
+  const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+  const backgroundImage = card.image ? `url(${API_BASE_URL}${card?.image?.url})` : `url(${card.imageUrl})`;
+
   return (
     <Link
       href={url}
       scroll={scroll}
       key={card.id}
       className={`relative bg-cover bg-center rounded-lg w-full ${index === length - 1 ? 'lg:w-full' : 'lg:max-w-72'} min-h-[250px]`}
-      style={{ backgroundImage: `url(${card.imageUrl})` }}
+      style={{ backgroundImage }}
     >
       <div className="absolute inset-0 bg-black bg-opacity-50 rounded-lg" />
       <div className="absolute bottom-0 z-10 p-4 text-white">
--- src/app/types.ts
+++ src/app/types.ts
@@ -1,10 +1,56 @@
+type ImageFormat = {
+  ext: string;
+  url: string;
+  hash: string;
+  mime: string;
+  name: string;
+  width: number;
+  height: number;
+  size: number;
+  path?: string | null;
+};
+
+type Image = {
+  alternativeText: string | null;
+  caption: string | null;
+  createdAt: string;
+  documentId: string;
+  ext: string;
+  formats: {
+    small?: ImageFormat;
+    thumbnail?: ImageFormat;
+  };
+  hash: string;
+  height: number;
+  id: number;
+  locale: string | null;
+  mime: string;
+  name: string;
+  previewUrl: string | null;
+  provider: string;
+  provider_metadata: any | null;
+  publishedAt: string;
+  size: number;
+  updatedAt: string;
+  url: string;
+  width: number;
+};
+
 export type Tour = {
-    id: string;
-    title: string;
-    description: string;
-    imageUrl: string;
-    departure: string;
-    returnTime: string;
-    included: string[];
-    notIncluded: string[];
-}
+  id: number;
+  documentId: string;
+  title: string;
+  image: Image;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  locale: string | null;
+  localizations: Array<any>;
+  description?: string;
+  departureTime?: string;
+  returnTime?: string;
+  included?: string[];
+  notIncluded?: string[];
+  imageUrl?: string;
+  hasCta?: string;
+};
