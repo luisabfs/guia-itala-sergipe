256ad295de0f60938f010bf006cd17b3033363f6
Luisa Barros <luisa.barros.dev@gmail.com>
2024-11-19 20:41:25 -0300

    feat: create whatsapp cta component

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import WhatsappCta from "../WhatsappCta";
 
 export default function Header() {
   return (
@@ -16,10 +17,7 @@ export default function Header() {
               <Link href="#tours">Roteiros</Link>
               <Image width={18} height={18}  src="/icons/external-light.svg" alt="Ícone do Whatsapp" />
             </div> */}
-            <Link target="_blank" className="flex mx-6 p-2 text-lg justify-center w-full gap-2 bg-[#1C4D42] font-bold rounded-full md:text-sm md:py-2 md:px-5" href="https://wa.me/557996411312?text=Oi%2C%20Itala!%20Vim%20pelo%20seu%20site%20e%20gostaria%20de%20planejar%20minha%20viagem%20para%20Sergipe%20com%20voc%C3%AA.%20Poderia%20me%20passar%20mais%20informa%C3%A7%C3%B5es%20sobre%20os%20passeios%20e%20roteiros%20tur%C3%ADsticos?">
-              <Image width={16} height={16}  src="/icons/whatsapp-light.svg" alt="Ícone do Whatsapp" />
-              <span>Fale comigo</span>
-            </Link>
+            <WhatsappCta />
         </nav>
     </div>
   );
