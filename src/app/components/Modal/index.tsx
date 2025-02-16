13dd2ecb5624145f184edf34b5cd4db50a7380e2
Luisa Barros <luisa.barros.dev@gmail.com>
2025-02-16 18:44:05 -0300

    fix: increase modal margins

--- src/app/components/Modal/index.tsx
+++ src/app/components/Modal/index.tsx
@@ -25,7 +25,7 @@ export default function Modal({ isOpen, title = 'Modal Title', onClose, children
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 cursor-pointer" onClick={onClose}>
-      <div className="bg-white rounded-lg p-6 w-full max-w-lg max-h-[calc(100vh-2rem)] overflow-y-auto m-4 cursor-auto" onClick={(e) => e.stopPropagation()}>
+      <div className="bg-white rounded-lg p-6 w-full max-w-lg max-h-[calc(100vh-2rem)] overflow-y-auto m-10 cursor-auto" onClick={(e) => e.stopPropagation()}>
         <div className="flex justify-between items-center mb-4 text-black">
           <h2 className="text-xl font-bold">{title}</h2>
           <button onClick={onClose} className="text-gray-500 text-xl hover:text-gray-800">&times;</button>
