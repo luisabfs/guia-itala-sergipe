9ba51bee6267092c3e01904e62a4a1849149081b
Luisa Barros <luisa.barros.dev@gmail.com>
2025-03-01 18:11:17 -0300

    refactor: change tour texts

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -7,18 +7,18 @@ import { usePathname } from "next/navigation";
 
 export default function Header() {
   const pathname = usePathname();
-  
+
   return (
     <div className="flex flex-col items-center w-full justify-between md:flex-row max-w-3xl lg:max-w-4xl xl:max-w-5xl">
-        <Link href="/" className="relative w-[220px] md:w-[240px] lg:w-[260px] h-28">
-          <Image fill objectFit="cover" src="/logos/logo-horizontal-white.png" alt="Logo Guia Itala" />
-        </Link>
-        <nav className="flex flex-col-reverse px-4 w-full justify-center md:w-fit items-center gap-4 md:flex-row lg:text-lg">
-          {/* {pathname !== '/' ? <div className="flex gap-1 items-center">
+      <Link href="/" className="relative w-[220px] md:w-[240px] lg:w-[260px] h-20 md:h-28">
+        <Image fill objectFit="cover" src="/logos/logo-horizontal-white.png" alt="Logo Guia Itala" />
+      </Link>
+      <nav className="flex flex-col-reverse px-4 w-full justify-center md:w-fit items-center gap-4 md:flex-row lg:text-lg">
+        {/* {pathname !== '/' ? <div className="flex gap-1 items-center">
             <Link href="/" className="hover:opacity-85" shallow>Início</Link>
           </div> : null} */}
-          <WhatsappCta />
-        </nav>
+        <WhatsappCta />
+      </nav>
     </div>
   );
 }
