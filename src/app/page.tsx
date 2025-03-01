5f8041048d0e13664a4aabaf7a79bf8a522e41d2
Luisa Barros <luisa.barros.dev@gmail.com>
2025-03-01 18:10:34 -0300

    refactor: change home background and title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
-import { 
+import {
   // Card, 
-  Header 
+  Header
 } from './components';
 import styles from './styles.module.css';
 // import { slugify } from './utils';
@@ -13,17 +13,16 @@ export default async function Home() {
       <section className={styles.backgroundWrapper}>
         <Header />
         <div className="flex flex-col w-full h-full justify-center items-center gap-4 md:gap-10">
-          <h1 className="whitespace-pre-line font-playfair font-bold text-3xl mt-12 md:mt-32 md:text-6xl">
-            Turismo para todos!
+          <h1 className="leading-tight whitespace-pre-line font-playfair font-bold drop-shadow-2xl text-3xl mt-10 md:mt-32 md:text-5xl">
+            Descubra Sergipe com uma <span className="bg-[#1c4d42] text-white px-2 md:my-2 lg:my-0 rounded-md inline-block">Especialista</span>
           </h1>
-          <span className="m-6 md:m-0 max-w-xl md:max-w-3xl lg:max-w-4xl xl:max-w-4xl text-sm lg:text-lg">
+          <span className="drop-shadow-xl m-6 md:m-0 max-w-xl md:max-w-3xl lg:max-w-4xl xl:max-w-4xl text-base lg:text-xl">
             Olá! Meu nome é Itala Aben-Athar, e sou Turismóloga, Agente de Viagens e Guia de Turismo em Aracaju, Sergipe, com <span className="font-bold">mais de 30 anos de experiência!</span> Tenho um profundo conhecimento da história, cultura e atrações locais, e estou pronta para proporcionar uma experiência personalizada para a sua viagem.
           </span>
         </div>
         <Link href="/roteiros" className="flex mt-4 p-2 px-6 text-lg justify-center gap-2 border-2 rounded-full backdrop-blur-md md:text-sm md:py-2 md:px-5 md:mt-12 hover:opacity-85">
           <span className="text-white font-bold text-sm md:text-lg">Ver roteiros</span>
         </Link>
-        <span className="flex text-sm font-medium self-start mt-auto text-[#4b7670]">Foto: Giovana Sabbatini</span>
       </section>
       {/* <section id="tours" className="flex flex-col h-full items-center gap-4 p-6 lg:p-6">
         <div className="flex flex-col w-full justify-center items-center py-6 gap-4 text-center">
